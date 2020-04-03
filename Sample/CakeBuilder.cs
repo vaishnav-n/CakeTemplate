fcb0de8793c0f59d1fcc22476d6a4e6a08c82a4c
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2020-04-03 11:50:53 +0530

    edits

--- Sample/CakeBuilder.cs
+++ Sample/CakeBuilder.cs
@@ -11,62 +11,25 @@ using Cake.Common;
 using Cake.Common.Tools.NuGet;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.IO;
-using Cake.Common.Diagnostics;
-using Cake.Npm.Install;
-using Cake.Npm;
-using Cake.Npm.RunScript;
 using Cake.Core.IO;
 
 namespace Sample
 {
     public static class CakeBuilder
     {
-        static string OctopusDeployUrl = string.Empty;
-        static string Target = string.Empty;
         static string BuildNumber = string.Empty;
-        static string MsBuildLogger = string.Empty;
-        static string OctopusDeployApiKey = string.Empty;
-        static string ProjectName = string.Empty;
-        static string ProcessesProjectName = string.Empty;
-        static string APIProjectName = string.Empty;
-        static string DeploymentBranches = string.Empty;
-        static string TeamCityBuildAgentDirectory = string.Empty;
-        static string NodeBackupPath = string.Empty;
         static string BranchName = string.Empty;
-        static string tenant = string.Empty;
-        static string solfilepath=string.Empty;
+
+
+        //[CakeMethodAlias]
+        //public static void TestingBuild(this ICakeContext context)
+        //{
+        //    context.MSBuild("./src/CakeDemo.sln");
+        //}
 
 
         [CakeMethodAlias]
-        public static void TestingBuild(this ICakeContext context)
-        {
-            context.MSBuild("./src/CakeDemo.sln");
-        }
-
-        [CakeMethodAlias]
-        public static void buildTasks(this ICakeContext context)
-        {
-           // OctopusDeployUrl = "https://deploy.hhaexchange.com";
-            //Target = context.Argument("target", "OctoRelease");
-            BuildNumber = context.ArgumentOrEnvironmentVariable("build.number", "", "0.0.1-local.0");
-           // OctopusDeployApiKey = context.ArgumentOrEnvironmentVariable("OctopusDeployApiKey", "");
-            MsBuildLogger = context.ArgumentOrEnvironmentVariable("MsBuildLogger", "", "");
-          //  ProjectName = context.ArgumentOrEnvironmentVariable("ProjectName", "", "ENT");
-          //  ProcessesProjectName = context.ArgumentOrEnvironmentVariable("ProcessesProjectName", "", "ENT Processes");
-          //  APIProjectName = context.ArgumentOrEnvironmentVariable("APIProjectName", "", "ENT.Internal.API");
-            DeploymentBranches = context.ArgumentOrEnvironmentVariable("DeploymentBranches", "", " ");
-            TeamCityBuildAgentDirectory = context.ArgumentOrEnvironmentVariable("teamcity.agent.home.dir", "", "c:\\BuildAgent");
-            NodeBackupPath = TeamCityBuildAgentDirectory + "\\node_backup\\ENTP\\node_modules";
-            BranchName = null;
-            tenant = null;
-
-
-            TaskBuild(context);
-            
-        }
-
-        [CakeMethodAlias]
-        public static void TaskBuild(ICakeContext context)
+        public static void TaskBuild(this ICakeContext context, string solutionfilepath,string buildoutputpath )
         {
            // Version(context);
            // RestorePackages(context);
@@ -76,54 +39,17 @@ namespace Sample
             var msBuildSettings = new MSBuildSettings()
                 .SetConfiguration("Release")
                 .WithProperty("DeployOnBuild", "true")
+                .WithProperty("OutDir", buildoutputpath)
                 .WithProperty("PublishProfile", path.ToString());
 
-            if (!string.IsNullOrEmpty(MsBuildLogger))
-            {
-                msBuildSettings.ArgumentCustomization = arguments =>
-                        arguments.Append(string.Format("/logger:{0}", MsBuildLogger));
-            }
 
-            context.MSBuild(solfilepath, msBuildSettings);
+            context.MSBuild(solutionfilepath, msBuildSettings);
 
-            //var npmRunSettings = new NpmRunScriptSettings()
-            //{
-            //    ScriptName = "build-prod",
-            //    WorkingDirectory = "ENT.UI/",
 
-            //};
-
-            //context.NpmRunScript(npmRunSettings);
-
-            ////Information("Backup node modules folder to " + NodeBackupPath);
-
-            //if (context.DirectoryExists("ENT.UI/node_modules"))
-            //{
-            //    if (context.DirectoryExists(NodeBackupPath))
-            //    {
-            //        //Information("Delete existing node modules backup");
-
-            //        context.DeleteDirectory(NodeBackupPath,
-            //            new DeleteDirectorySettings
-            //            {
-            //                Recursive = true
-            //            });
-            //    }
-
-            //    //Information("Move working node modules to backup folder");
-            //    try
-            //    {
-            //        context.MoveDirectory("ENT.UI/node_modules", NodeBackupPath);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        context.Warning("Failed to move node modules to backup " + ex.ToString());
-            //    }
-            //}
         }
 
         [CakeMethodAlias]
-        public static void Version(ICakeContext context)
+        public static void Version(this ICakeContext context)
         {
             GitVersionSettings buildServerSettings = new GitVersionSettings
             {
@@ -156,48 +82,7 @@ namespace Sample
             //Information("BuildNumber updated: " + BuildNumber);
         }
 
-        //public static void RestorePackages(ICakeContext context)
-        //{
-        //    context.NuGetRestore("ENT.sln");
-
-        //    context.DotNetCoreRestore("ENT.sln");
-
-        //    //Information("Restore node modules folder");
-
-        //    if (context.DirectoryExists(NodeBackupPath))
-        //    {
-        //        if (context.DirectoryExists("./ENT.UI/node_modules"))
-        //        {
-        //            //Information("Delete existing node modules folder");
-
-        //            context.DeleteDirectory("./ENT.UI/node_modules",
-        //                new DeleteDirectorySettings
-        //                {
-        //                    Recursive = true
-        //                });
-        //        }
-
-        //        //Information("Move backup node modules folder to working directory");
-
-        //        try
-        //        {
-        //            context.MoveDirectory(NodeBackupPath, "./ENT.UI/node_modules");
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            context.Warning("Failed to move node backup into project" + ex.ToString());
-        //        }
-        //    }
-
-        //    var npmInstallSettings = new NpmInstallSettings()
-        //    {
-        //        LogLevel = NpmLogLevel.Verbose,
-        //        WorkingDirectory = "ENT.UI/"
-        //    };
-
-        //    context.NpmInstall(npmInstallSettings);
-        //}
-
+        
         public static bool IsFeatureBranchWithTenant(ICakeContext context, string DeploymentBranches, string BranchName, string tenant)
         {
             //	Information("Deployment Branches are: " + DeploymentBranches);
